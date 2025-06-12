4fa366825ab4023a0de2cf7db6e135631c58e6af
Analista Mineria de Datos <Adatos@mic.local>
2025-06-12 14:43:41 -0500

    Cambios inteligencia

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ import streamlit as st
 st.title("Demo de Streamlit en equipo")
 st.write("¡Hola! Esta es una aplicación simple hecha con Streamlit.")
 st.write("Practiquemos GitHub juntos.")
+
+
+st.write('-----')
+st.subheader("Juputa yo juego porque juego")
+st.write('-----')
